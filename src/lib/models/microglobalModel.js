d4113d1ef83290132b4b173ff0cf9de5387718eb
santiclfriedrich <santiclear1@hotmail.com>
2026-04-08 15:28:23 -0300

    Fix: imagen Microglobal microg.jpg

--- src/lib/models/microglobalModel.js
+++ src/lib/models/microglobalModel.js
@@ -28,7 +28,7 @@ export function formatMicroglobalProducts(rawProducts) {
         stockLevel: stock > 20 ? "disponible" : stock > 0 ? "limitado" : "sin stock",
         stockTotal: stock,
         link:       "",
-        image:      "/micro.jpg",
+        image:      "/microg.jpg",
         provider:   "Microglobal",
       };
     })
